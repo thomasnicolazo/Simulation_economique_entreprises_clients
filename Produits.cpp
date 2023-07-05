a7799d0e11f82b8fd4b5df71425022fdf44b71d3
thomas <thomas.nicolazo@gmail.com>
2023-07-06 00:13:28 +0200

    Define methods for class Produit, ProduitFini and ProduitBrute

---
+++ Produits.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include "Produits.hpp"
+
+// class Produits
+	
+Produits::Produits(std::string nom,int dureeF){
+	dureeFabrication = dureeF;
+	fabriqueDepuis = 0;
+	estFabrique = false;
+	name = nom;
+}
+
+Produits::~Produits(){}
+
+void Produits::setPrix(double price){
+		prix = price;
+}
+
+double Produits::getPrix(){
+	return prix;
+}
+
+void Produits::setDureedeFabrication (int dureeF){
+	dureeFabrication = dureeF;
+}
+
+int Produits::getDureeDeFabrication(){
+	return dureeFabrication;
+}
+
+void Produits::updateFabriqueDepuis(){
+	fabriqueDepuis++;
+	if(fabriqueDepuis == dureeFabrication){
+		estFabrique = true;
+	}
+}
+	
+
+
+// class ProduitBrute
+
+ProduitBrute::ProduitBrute(std::string _nom,int _dureeF) : Produits::Produits(std::string _nom,int _dureeF)
+{
+	dureeFabrication = _dureeF;
+	fabriqueDepuis = 0;
+	estFabrique = false;
+	name = _nom;
+}
+	
+ProduitBrute::~ProduitBrute(){}
+
+
+// class ProduitFini
+
+ProduitFini::ProduitFini(std::string _nom,int _dureeF, int quality) : Produits::Produits(std::string _nom,int _dureeF)
+{
+	dureeFabrication = _dureeF;
+	fabriqueDepuis = 0;
+	estFabrique = false;
+	name = _nom;
+	qualite = quality;
+}
+
+ProduitFini::~ProduitFini(){}
+
+int ProduitFini::getQualite(){
+		return qualite;
+}
+
