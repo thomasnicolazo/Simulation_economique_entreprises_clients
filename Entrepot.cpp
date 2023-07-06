e4d04774dc2a3fc6f7b1f3a58dc5851f1bcd5221
thomas <thomas.nicolazo@gmail.com>
2023-07-06 13:37:22 +0200

    replace attributes estFabrique with the method is done in the function getProductsReadyToSell

--- Entrepot.cpp
+++ Entrepot.cpp
@@ -33,9 +33,34 @@ Produits* Entrepot::getProductWithLowerPrice(std::string name, std::vector<Produ
 std::vector<Produits> Entrepot::getProductsReadyToSell(){
 	std::vector<Produits> productsReadyToSell;
 	for(int i=0; i<vectorProduitFini.size();i++){
-		if(vectorProduitFini[i].estFabrique){
-			productsReadyToSell.push_back(vectorProduitFini);
+		if(vectorProduitFini[i].isDone()){
+			productsReadyToSell.push_back(vectorProduitFini[i]);
 		}
 	}
 	return productsReadyToSell;
+}
+
+void Entrepot::productHasBeenBought(std::string name,double price){
+	int confirmation =0;
+	for(int i=0; i<vectorProduitFini.size();i++){
+		if(vectorProduitFini[i].isDone()){
+			if(vectorProduitFini[i].getPrix() == price && vectorProduitFini[i].getName() == name){
+				vectorProduitFini.erase(vectorProduitFini.begin()+i);
+				confirmation=1;
+				break;
+			}
+		}
+	}
+	if(confirmation == 0){
+		std::cout <<"objet non detruit dans entrepot" <<std::endl;
+	}
+}
+
+void Entrepot::updateEntrepot(){
+	for(int i=0;i<vectorProduitFini.size();i++){
+		vectorProduitFini[i].updateFabriqueDepuis();
+	}
+	for(int i=0;i<vectorProduitBrute.size();i++){
+		vectorProduitBrute[i].updateFabriqueDepuis();
+	}
 }
