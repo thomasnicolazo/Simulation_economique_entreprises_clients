c0d09fc314df1a4adaee4039dcda4f06cac32633
jean25marc <jm.prudham@gmail.com>
2023-07-06 14:02:35 +0200

    Update ComportementEntreprise.cpp

--- ComportementEntreprise.cpp
+++ ComportementEntreprise.cpp
@@ -6,9 +6,14 @@ ComportementEntreprise::~ComportementEntreprise()=default;
 
 void ComportementEntreprise::action(){
     /**
-     * 
+     * une entreprise achete si elle ne produit plus
+     * Une entreprise produit tant qu'elle a les produits pour fabrique le produit fabrications
+     * Sinon elle s'arrête
+     * Une entreprise achete le minimum de produit pour produire une unité de produit fini 
+     * Si elle lui reste un acompte r&d...?
      * 
      * 
     */
+   //this->acteur->
 
 }
