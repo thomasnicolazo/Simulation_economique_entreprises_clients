847e5e134630479a1307205caa6d6bb890d38813
thomas <thomas.nicolazo@gmail.com>
2023-07-07 11:52:04 +0200

    add nameEntreprise to the attributes of the Produits class, add argument to set  nomEntreprise in the constructor of the Produits class

--- Produits.cpp
+++ Produits.cpp
@@ -4,11 +4,12 @@
 
 // class Produits
 	
-Produits::Produits(std::string nom,int dureeF){ //constructeur  Produits
+Produits::Produits(std::string nom,int dureeF, std::string nomEntreprise ){ //constructeur  Produits
 	dureeFabrication = dureeF;	//inittialise le temps de fabrication total du produit, futur developpement un meme type produit pourra avoir different temps de fabrication dû àddes problèmes.
 	fabriqueDepuis = 0; // durée depuis laquelle l'objet est en construction
 	estFabrique = false; // l'objet est il fini de construire
 	name = nom; // nom du produit // type de produit (Ex:pile)
+	nameEntreprise = nomEntreprise; // nom de l'entreprise qui a fabriqué le produit
 }
 
 Produits::~Produits(){}
@@ -53,12 +54,13 @@ void Produits::displayProduct(){
 
 // class ProduitBrute
 
-ProduitBrute::ProduitBrute(std::string _nom,int _dureeF) : Produits::Produits( _nom, _dureeF)
+ProduitBrute::ProduitBrute(std::string _nom,int _dureeF) : Produits::Produits( _nom, _dureeF, _nomEntreprise)
 {
 	dureeFabrication = _dureeF;
 	fabriqueDepuis = 0;
 	estFabrique = false;
 	name = _nom;
+	nameEntreprise = _nomEntreprise;
 }
 	
 ProduitBrute::~ProduitBrute(){}
@@ -66,12 +68,13 @@ ProduitBrute::~ProduitBrute(){}
 
 // class ProduitFini
 
-ProduitFini::ProduitFini(std::string _nom,int _dureeF, int quality) : Produits::Produits( _nom, _dureeF)
+ProduitFini::ProduitFini(std::string _nom,int _dureeF, int quality, std::string _nomEntreprise) : Produits::Produits( _nom, _dureeF, _nomEntreprise)
 {
 	dureeFabrication = _dureeF;
 	fabriqueDepuis = 0;
 	estFabrique = false;
 	name = _nom;
+	nameEntreprise = _nomEntreprise;
 	qualite = quality; // pas utilisé/implémenté pour le moment, un meme type de produit pourra avoir differentes qualités (peut etre un prix qui varira en fonction de cette qualité aussi)
 }
 
